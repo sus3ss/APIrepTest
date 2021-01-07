925ed9e4bd2b5f2e96934293f65e76fe76789623
sus3ss <Skadaiahnamath@global.internal>
2021-01-07 10:42:22 +0530

    commit

---
+++ src/test/java/stepDefinations/Hooks.java
@@ -0,0 +1,26 @@
+package stepDefinations;
+
+import java.io.IOException;
+
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+	@Before("@DeletePlace")
+	public void beforeScenario() throws IOException
+	{		//execute this code only when place id is null
+		//write a code that will give you place id
+		
+		StepDefination m =new StepDefination();
+		if(StepDefination.place_id==null)
+		{
+		
+		m.add_Place_Payload_with("Sushanth", "French", "Asia");
+		m.user_calls_with_http_request("AddPlaceAPI", "POST");
+		m.verify_place_Id_created_maps_to_using("Sushanth", "getPlaceAPI");
+		}
+		
+		
+
+	}
+}
