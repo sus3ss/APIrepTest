f9eb4312e382e3e46f0404a95aa8d8eb4e0ab724
sus3ss <68944585+sus3ss@users.noreply.github.com>
2021-01-08 15:23:47 +0530

    Update APIResources.java
    Correct

--- src/test/java/resources/APIResources.java
+++ src/test/java/resources/APIResources.java
@@ -4,7 +4,7 @@ public enum APIResources {
 	
 	AddPlaceAPI("/maps/api/place/add/json"),
 	getPlaceAPI("/maps/api/place/get/json"),
-	deletePlaceAPI("/maps/api/place/delete1/json");
+	deletePlaceAPI("/maps/api/place/delete/json");
 	private String resource;
 	
 	APIResources(String resource)
