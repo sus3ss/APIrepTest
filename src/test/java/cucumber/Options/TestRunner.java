9f194555608028fba3bd6de056666bab00d64c2d
sushanth <sushanth.math@gmail.com>
2021-02-05 23:45:19 +0530

    test commit

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinations"},tags= "@DeletePlace")
+@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinations"},tags= "@Regression")
 public class TestRunner {
 //tags= {"@DeletePlace"}  compile test verify
 }
