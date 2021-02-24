37ff56c96cab6b918aed1358f78095ee0fe89917
sus3ss <skadaiahnamath@BLR-LAP-015.global.internal>
2021-02-24 15:36:55 +0530

    test

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -17,7 +17,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Utils {
-
+//test
 	public static RequestSpecification req;
 	public RequestSpecification requestSpecification() throws IOException
 	{
